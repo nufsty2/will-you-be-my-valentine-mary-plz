0e226d30fa3524b92af4d55ee1c353811ef80e51
Jeff DeGraw <jeff.degraw@liveviewtech.com>
2023-02-13 17:51:46 -0700

    input string working

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,43 @@
 class PagesController < ApplicationController
+  @@count = 0
+
   def home
     @input_string = params[:input_string]
+    @result = ""
+    @status = ""
+
+    if !@input_string.nil?
+      puts "count = #{@@count}"
+
+      @input_string.downcase!
+
+      input_string_parts = @input_string.split
+
+      phrases = parse_phrases
+
+      phrases.each do |phrase|
+        if (input_string_parts - phrase).size > 1
+          @@count += 1
+          @result = "MATCH!"
+          @status = "#{@@count} of 9 phrases found!"
+          break
+        end
+
+        @result = "Try again..."
+      end
+
+      if @@count == 9
+        @result = "YOU WIN! HERE IS THE SECRET MESSAGE:"
+        @status = "I love you so much, Mar."
+      end
+    end
+
+    puts "GOt here!"
+
+    render
+  end
+
+  def parse_phrases
+    I18n.t(:phrases).map { |phrase| phrase.split }
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
   root to: "pages#home"
+  post '/pages/home', to: "pages#home"
 end
